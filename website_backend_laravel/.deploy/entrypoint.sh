036fb7a578ddd5bc326111aa21a56d5c23dab9a5
samjidiamond <odejinmisamuel@gmail.com>
2025-12-28 17:22:43 +0100

    Fixing website deployment

--- website_backend_laravel/.deploy/entrypoint.sh
+++ website_backend_laravel/.deploy/entrypoint.sh
@@ -2,14 +2,14 @@
 
 echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
 
-chmod 777 storage bootstrap
+cd $LARAVEL_PATH && chmod 777 -R storage bootstrap
 
-composer dump-autoload --no-interaction --no-dev --optimize
+cd $LARAVEL_PATH && composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
 
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
-php artisan migrate --no-interaction --force
+cd $LARAVEL_PATH && php artisan migrate --no-interaction --force
 
 echo "🎬 start supervisord"
 
