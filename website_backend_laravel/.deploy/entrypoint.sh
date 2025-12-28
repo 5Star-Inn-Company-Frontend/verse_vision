5abecf3ebab4bb9fcccc451f0eadbbfc6d84fce9
samjidiamond <odejinmisamuel@gmail.com>
2025-12-28 17:45:13 +0100

    Fixing website deployment

--- website_backend_laravel/.deploy/entrypoint.sh
+++ website_backend_laravel/.deploy/entrypoint.sh
@@ -7,12 +7,11 @@ ls
 ls $LARAVEL_PATH
 
 cd $LARAVEL_PATH && composer dump-autoload --no-interaction --no-dev --optimize
-composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
 
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
-php artisan migrate --no-interaction --force
+cd $LARAVEL_PATH && php artisan migrate --no-interaction --force
 
 echo "🎬 start supervisord"
 
